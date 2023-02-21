c7be8d2ee242ef2f6c57df512551a24ef30daeab
guardrails-test <63639880+guardrails-test@users.noreply.github.com>
2023-02-21 15:42:28 +0700

    Create dangerous-js-file.js

---
+++ dangerous-js-file.js
@@ -0,0 +1,1 @@
+console.log("calling dangerousFunction() with static string");var userInput = req.body.danger;lib.dangerousFunction("static input is fine"); // compliantconsole.log("calling dangerousFunction() with user input");lib.dangerousFunction(userInput); // non-compliantconsole.log("dangerousFunction() called successfully");
